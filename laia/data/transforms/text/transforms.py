b5817595e7dd71500bde52e8c76df30aeae8be3e
starride-teklia <starride@teklia.com>
2024-08-12 11:30:14 +0000

    Document tokenize/untokenize functions

--- laia/data/transforms/text/transforms.py
+++ laia/data/transforms/text/transforms.py
@@ -10,9 +10,27 @@ _logger = log.get_logger(__name__)
 
 
 def untokenize(
-    sentence: str, space_token: str = "<space>", space_display: str = " "
+    tokenized_sentence: str, space_token: str = "<space>", space_display: str = " "
 ) -> str:
-    return "".join(sentence.split()).replace(space_token, space_display)
+    """
+    Untokenize the input text from PyLaia format.
+
+
+    Args:
+        tokenized_sentence (str): The tokenized text to untokenize.
+        space_token (str, optional): The token to use for spaces in the tokenized string. Defaults to "<space>".
+        space_display (str, optional): The token used to represent spaces in the input string. Defaults to " ".
+
+    Returns:
+        str: The untokenized text.
+
+    Example:
+        untokenize("T h i s <space> i s <space> a n <space> e x a m p l e")
+        >>> "This is an example"
+        untokenize("T h e <space> O l y m p i c s <space> G a m e s <space> a r e <space> o r g a n i z e d <space> i n <space> <location>P a r i s")
+        >>> "The Olympics Games are organized in <location>Paris"
+    """
+    return "".join(tokenized_sentence.split()).replace(space_token, space_display)
 
 
 def tokenize(
@@ -21,6 +39,24 @@ def tokenize(
     space_display: str = " ",
     symbols: set = {},
 ) -> str:
+    """
+    Tokenize the input text in PyLaia format.
+
+    Args:
+        sentence (str): The input text.
+        space_token (str, optional): The token to use for spaces in the tokenized string. Defaults to "<space>".
+        space_display (str, optional): The token used to represent spaces in the input string. Defaults to " ".
+        symbols (set, optional): The set of symbols from PyLaia vocabulary. Defaults to {}.
+
+    Returns:
+        str: The tokenized text with spaces replaced by `space_display`.
+
+    Example:
+        untokenize("This is an example")
+        >>> "T h i s <space> i s <space> a n <space> e x a m p l e"
+        tokenize("The Olympics Games are organized in <location>Paris")
+        >>> "T h e <space> O l y m p i c s <space> G a m e s <space> a r e <space> o r g a n i z e d <space> i n <space> <location>P a r i s"
+    """
     pattern = re.compile(rf"{'|'.join(f'({re.escape(word)})' for word in symbols)}")
     return " ".join(
         [
